0e7034f9920d5c773e89f5f20f9dd687f4df4798
shubhsk88 <shubham88singh@gmail.com>
2020-05-05 03:54:50 +0530

    Adding the input and output patterns for the game

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,27 @@
-puts 'Hello World'
+puts "Welcome to the game of Tictoe"
+
+puts "Instructions"
+
+puts "1: This game should be played with two players"
+puts "2: Every turns you have to choose the number using your chances"
+puts "3: The first player start with o and the other player will have x"
+puts "4: The first person that matches the correct pattern wins the game"
+puts "\n\n"
+
+puts("\t1 | 2 | 3 ")
+puts ("\t---------")
+puts("\t4 | 5 | 6 ")
+puts ("\t---------")
+puts("\t7 | 8 | 9 ")
+puts ("\n")
+puts ("Player1: Which is next move")
+
+puts ("\n")
+gets.chomp()
+puts ("\n")
+puts ("You have chosen number 5")
+
+puts ("\n")
+
+puts ("Player 1 wins")
+
