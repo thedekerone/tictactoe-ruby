e2aa7170b404d7d889c31c54dd8d52132b0fb377
thedekerone <mauriciofow@gmail.com>
2020-05-05 15:26:52 -0500

    Player implementation

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -2,34 +2,19 @@ class Player
 
     attr_accessor :moves
 
+    @@winner=false;
+    @@board=[1,2,3,4,5,6,7,8,9]
+
     def initialize(sign,name)
         @sign=sign
         @moves = [] 
         @name=name
-
-
     end
     
     def draw
-        p "Enter the #{@name} move"
-
        make_move()
-        p @@board
-    end
-    
-    
-    @@winner=false;
-    @@board=[1,2,3,4,5,6,7,8,9]
-
-    def make_move
-        while 1
-           
-            move = gets.chomp().to_i
-            choose_number(move)
-            next if(move>9 or move <1)
-            break;
-        end
-    end
+       draw_board()
+    end 
 
     def isWinner
         c=@@board
@@ -44,42 +29,67 @@ class Player
          
         return @@winner
     end
+
+    private
+
+    def draw_board
+        puts("\t#{@@board[0]} | #{@@board[1]} | #{@@board[2]} ")
+        puts ("\t---------")
+        puts("\t#{@@board[3]} | #{@@board[4]} | #{@@board[5]} ")
+        puts ("\t---------")
+        puts("\t#{@@board[6]} | #{@@board[7]} | #{@@board[8]} ")
+        puts ("\n")
+    end   
+
+    def make_move
+        while 1
+            puts "Enter the #{@name} move"
+            move = gets.chomp().to_i
+            is_valid = choose_number(move)
+            if(!is_valid)
+                puts "\nWrong input, try again!\n"
+                next
+            end
+            break;
+        end
+    end
+
     def choose_number(move)
+        valid=false
         @@board = @@board.map do |number|
-            
             if(number==move)
                @moves.push(number)
-                @sign
+               valid=true
+               @sign
             else
                 number
             end
         end
+        valid
     end
     
-
-
-
 end
 
-player1 = Player.new("x","player1")
-player2 = Player.new("o","player2")
 
+def start_game
+    player1 = Player.new("x","player1")
+    player2 = Player.new("o","player2")
+    while 1
+        player1.draw()
+            if(player1.isWinner())  
 
+                puts "Player 1 Wins"
+                break
+            end
 
-while 1
-
-player1.draw()
- if(player1.isWinner())  
-
-    puts "Player 1 Wins"
-    break
- end
-player2.draw()
-
- 
-if(player2.isWinner())
-    puts "Player 2 wins"
-    break
-end 
-p player1.moves
+            player2.draw()  
+        
+            if(player2.isWinner())
+                puts "Player 2 wins"
+                break
+            end 
+        end
 end
+
+
+start_game()
