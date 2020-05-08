c85772677ffc4925db661bee180c475b54232e44
thedekerone <mauriciofow@gmail.com>
2020-05-08 11:13:59 -0500

    improved user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,4 @@
-# !/usr/bin/env ruby
+#!/usr/bin/env ruby
 
 puts 'Welcome to the game of Tictoe'
 
@@ -8,21 +8,71 @@ puts '1: This game should be played with two players'
 puts '2: Every turns you have to choose the number using your chances'
 puts '3: The first player start with o and the other player will have x'
 puts '4: The first person that matches the correct pattern wins the game'
+puts '5: Enter the letter "e" to end the game'
 puts "\n\n"
 
-puts("\t1 | 2 | 3 ")
-puts "\t---------"
-puts("\t4 | 5 | 6 ")
-puts "\t---------"
-puts("\t7 | 8 | 9 ")
-puts "\n"
-puts 'Player1: Which is next move'
+loop do # rubocop:disable Metrics/BlockLength
+  loop do
+    puts 'DRAW BOARD WITH POSSIBLE MOVES'
 
-puts "\n"
-move = gets.chomp
-puts "\n"
-puts "You have chosen number #{move}"
+    puts 'Player1 select move'
 
-puts "\n"
+    puts "\n"
+    move = gets.chomp.to_i
+    puts "\n"
+    puts "You have chosen number #{move}"
 
-puts 'Player 1 wins'
+    puts 'verify if move is legal'
+    puts "\n"
+
+    if move < 10 and move.positive?
+      puts 'player move is displayed'
+      puts "\n"
+      break
+    else
+      puts 'Wrong move, please select a valid move'
+      puts "\n"
+
+    end
+  end
+  puts 'Enter e if you want to finish'
+  puts "\n"
+
+  finish = gets.chomp
+  if finish == 'e'
+    puts 'Player 1 wins the game'
+    break
+  end
+  loop do
+    puts 'DRAW BOARD WITH POSSIBLE MOVES'
+
+    puts 'Player2 select move'
+
+    puts "\n"
+    move = gets.chomp.to_i
+    puts "\n"
+    puts "You have chosen number #{move}"
+
+    puts 'verify if move is legal'
+    puts "\n"
+
+    if move < 10 and move.positive?
+      puts 'player move is displayed'
+      puts "\n"
+      break
+    else
+      puts 'Wrong move, please select a valid move'
+      puts "\n"
+
+    end
+  end
+
+  puts 'Enter e if you want to finish'
+  puts "\n"
+
+  finish = gets.chomp
+  if finish == 'e'
+    puts 'Player 2 wins the game'
+    break
+  end
+end
