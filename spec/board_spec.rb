b263d0679aac8ad66bc7bcba6765ec82bd730a57
shubhsk88 <shubham88singh@gmail.com>
2020-05-14 02:00:15 +0530

    Adding the tests for the tictac toe game

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,12 @@
 require_relative '../lib/board.rb'
-
+# rubocop:disable Layout/LineLength
 describe Board do
-    let(:test_board) { Board.new }
-  describe "#draw_board" do
-      it "returns a board with the elements on the array" do
-          expect(test_board.draw_board).to eql("\t1 | 2 | 3\n      \t---------\n      \t4 | 5 | 6\n      \t---------\n      \t7 | 8 | 9")
-      end
+  let(:test_board) { Board.new }
+  describe '#draw_board' do
+    it 'returns a board with the elements on the array' do
+      expect(test_board.draw_board).to eql("\t1 | 2 | 3\n      \t---------\n      \t4 | 5 | 6\n      \t---------\n      \t7 | 8 | 9")
+    end
   end
-
 end
+
+# rubocop:enable Layout/LineLength
